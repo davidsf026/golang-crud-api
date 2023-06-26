14bb8a2076d90a407a13e764022b9a697052600f
dsferreira54 <david.sf026@gmail.com>
2023-06-26 09:03:09 -0300

    Defining controller model

--- go.mod
+++ go.mod
@@ -2,9 +2,8 @@ module github.com/dsferreira54/golang-crud-api
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1
-
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,25 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/dsferreira54/golang-crud-api/src/configuration/rest_err"
+	"github.com/dsferreira54/golang-crud-api/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
 
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("The are some incorrect fields, error=%s", err.Error()),
+		)
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
